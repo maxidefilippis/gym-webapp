f9727dd50b7f0da63a68530b147a770eefbb5aca
maxidefilippis <maxidefilippis@gmail.com>
2021-06-08 23:50:16 -0300

    [FEATURE] Create Main Layout

---
+++ src/pages/Layout/mainLayout.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./mainLayout.scss";
+
+const MainLayout = () => {
+  return (
+    <div className="mainLayoutContainer">
+      <Sidebar />
+      <header className="mainLayoutContainer__header">
+        <Navbar
+          className="mainLayoutContainer__header__navbar"
+          drawerToggleClick={drawerToggleClick}
+        />
+        <SideDrawer show={sideDraweOpen} />
+      </header>
+      <section className={`mainLayoutContainer__content ${classContent}`}>
+        {children}
+      </section>
+    </div>
+  );
+};
+
+export default MainLayout;
