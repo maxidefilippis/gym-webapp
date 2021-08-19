16d5823aa140774758762520997d588f3ff28fb6
maxidefilippis <maxidefilippis@gmail.com>
2021-08-18 23:58:30 -0300

    [UPDATE] Homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,7 +14,14 @@ const Home = () => {
     <MainLayout>
       <div className="home__container">
         <h1 className="home__title">BIENVENIDO!</h1>
-        <p>Hola {user}!</p>
+        <p className="home__user">
+          Hola <b>{user}</b>!
+        </p>
+        <div className="home__buttons">
+          <p>Perfil</p>
+          <p>Alumnos</p>
+          <p>Rutinas</p>
+        </div>
         <div className="login__actions">
           <span onClick={logout}>Cerrar sesión</span>
         </div>
